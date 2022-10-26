360bec62c7089660e504fdbf0f9bd9e5afa45cc5
Markus Eyting <eyting.ma@gmail.com>
2022-10-26 10:56:34 +0200

    rank like refresh but with grey

--- rank/__init__.py
+++ rank/__init__.py
@@ -21,31 +21,31 @@ df2.realeffort = df2.realeffort+df2["random"]
 df1['race'] = df1['race'].replace({'Hispanic or Latin':'Hispanic'})
 df2['race'] = df2['race'].replace({'Hispanic or Latin':'Hispanic'})
 
-df1h = df1[df1.race=="Hispanic"].reset_index()
-df1a = df1[df1.race=="Asian"].reset_index()
-df2h = df2[df2.race=="Hispanic"].reset_index()
-df2a = df2[df2.race=="Asian"].reset_index()
+df1f = df1[df1.gender=="female"].reset_index()
+df1m = df1[df1.gender=="male"].reset_index()
+df2f = df2[df2.gender=="female"].reset_index()
+df2m = df2[df2.gender=="male"].reset_index()
 
-df1h = df1h[["name", "gender", "matrices", "race"]].sample(n=12, random_state = 1)
-df1a = df1a[["name", "gender", "matrices", "race"]].sample(n=12, random_state = 1)
-df2h = df2h[["name", "gender", "realeffort", "race"]].sample(n=12, random_state = 1)
-df2a = df2a[["name", "gender", "realeffort", "race"]].sample(n=12, random_state = 1)
+df1f = df1f[["name", "gender", "matrices"]].sample(n=12, random_state = 1)
+df1m = df1m[["name", "gender", "matrices",]].sample(n=12, random_state = 1)
+df2f = df2f[["name", "gender", "realeffort"]].sample(n=12, random_state = 1)
+df2m = df2m[["name", "gender", "realeffort"]].sample(n=12, random_state = 1)
 
-df1h["mat_rank"] = df1h.matrices.rank(ascending=False)
-df1a["mat_rank"] = df1a.matrices.rank(ascending=False)
-df2h["re_rank"] = df2h.realeffort.rank(ascending=False)
-df2a["re_rank"] = df2a.realeffort.rank(ascending=False)
+df1m["mat_rank"] = df1m.matrices.rank(ascending=False)
+df1f["mat_rank"] = df1f.matrices.rank(ascending=False)
+df2m["re_rank"] = df2m.realeffort.rank(ascending=False)
+df2f["re_rank"] = df2f.realeffort.rank(ascending=False)
 
-df1h = df1h.sort_values(by=['mat_rank']).reset_index()
-df1a = df1a.sort_values(by=['mat_rank']).reset_index()
-df2h = df2h.sort_values(by=['re_rank']).reset_index()
-df2a = df2a.sort_values(by=['re_rank']).reset_index()
+df1m = df1m.sort_values(by=['mat_rank']).reset_index()
+df1f = df1f.sort_values(by=['mat_rank']).reset_index()
+df2m = df2m.sort_values(by=['re_rank']).reset_index()
+df2f = df2f.sort_values(by=['re_rank']).reset_index()
 
+df1f.to_csv('_static/global/rankrankings/workers_rank_mat_female.csv')
+df1m.to_csv('_static/global/rankrankings/workers_rank_mat_male.csv')
+df2f.to_csv('_static/global/rankrankings/workers_rank_re_female.csv')
+df2m.to_csv('_static/global/rankrankings/workers_rank_re_male.csv')
 
-df1h.to_csv('_static/global/rankrankings/workers_rank_mat_hispanic.csv')
-df1a.to_csv('_static/global/rankrankings/workers_rank_mat_asian.csv')
-df2h.to_csv('_static/global/rankrankings/workers_rank_re_hispanic.csv')
-df2a.to_csv('_static/global/rankrankings/workers_rank_re_asian.csv')
 
 class C(BaseConstants):
     NAME_IN_URL = 'rank'
@@ -59,34 +59,33 @@ class C(BaseConstants):
     examplescore = 5
     examplebonus = examplescore * conversionrate
 
-    names_mat_a = [{'name': df1a['name'][i],
-                    'gender': df1a['gender'][i],
-                    'mat_rank': df1a["mat_rank"][i],
-                    'race': df1a["race"][i],
+    names_mat_f = [{'name': df1f['name'][i],
+                    'gender': df1f['gender'][i],
+                    'mat_rank': df1f["mat_rank"][i],
+                    'race': df1f["race"][i],
                      }
-                  for i in range(len(df1a))]
+                  for i in range(len(df1f))]
 
-    names_mat_h = [{'name': df1h['name'][i],
-                   'gender': df1h['gender'][i],
-                   'mat_rank': df1h["mat_rank"][i],
-                   'race': df1h["race"][i],
-
+    names_mat_m = [{'name': df1m['name'][i],
+                   'gender': df1m['gender'][i],
+                   'mat_rank': df1m["mat_rank"][i],
+                   'race': df1m["race"][i],
                     }
-                  for i in range(len(df1h))]
+                  for i in range(len(df1m))]
 
-    names_re_a = [{'name': df2a['name'][i],
-                   'gender': df2a['gender'][i],
-                   're_rank': df2a["re_rank"][i],
-                   'race': df2a["race"][i],
+    names_re_f = [{'name': df2f['name'][i],
+                   'gender': df2f['gender'][i],
+                   're_rank': df2f["re_rank"][i],
+                   'race': df2f["race"][i],
                    }
-                  for i in range(len(df2a))]
+                  for i in range(len(df2f))]
 
-    names_re_h = [{'name': df2h['name'][i],
-                   'gender': df2h['gender'][i],
-                   're_rank': df2h["re_rank"][i],
-                   'race': df2h["race"][i],
+    names_re_m = [{'name': df2m['name'][i],
+                   'gender': df2m['gender'][i],
+                   're_rank': df2m["re_rank"][i],
+                   'race': df2m["race"][i],
                    }
-                  for i in range(len(df2h))]
+                  for i in range(len(df2m))]
 
 class Subsession(BaseSubsession):
     pass
@@ -100,11 +99,11 @@ class Player(BasePlayer):
     ranking = models.CharField(blank=True)
     check = models.CharField(blank=True)
     abcdleft = models.CharField(blank=True)
-    asiansleft = models.CharField(blank=True)
-    asianranking_mat = models.CharField()
-    asianranking_re = models.CharField()
-    hispanicranking_mat = models.CharField()
-    hispanicranking_re = models.CharField()
+    malesleft = models.CharField(blank=True)
+    femaleranking_mat = models.CharField()
+    femaleranking_re = models.CharField()
+    maleranking_mat = models.CharField()
+    maleranking_re = models.CharField()
 
 
 class check3(Page):
@@ -128,13 +127,13 @@ class instructions_rank(Page):
 
 class rank3(Page):
     form_model = 'player'
-    form_fields = ['ranking', 'asiansleft']
+    form_fields = ['ranking', 'malesleft']
 
     def vars_for_template(player):
-        player.hispanicranking_mat = str(C.names_mat_h)
-        player.asianranking_mat = str(C.names_mat_a)
-        player.hispanicranking_re = str(C.names_re_h)
-        player.asianranking_re = str(C.names_re_a)
+        player.femaleranking_mat = str(C.names_mat_f)
+        player.maleranking_mat = str(C.names_mat_m)
+        player.femaleranking_re = str(C.names_re_f)
+        player.maleranking_re = str(C.names_re_m)
 
     def error_message(player, values):
         rankinglist = values["ranking"].split(",")
@@ -142,38 +141,39 @@ class rank3(Page):
             print(rankinglist)
             return 'Please add all workers to the mixed ranking.'
 
+
     def js_vars(player):
         if player.participant.task == 'logic':
-            names_h = [C.names_mat_h[i]["name"] for i in range(len(C.names_mat_h))]
-            names_a = [C.names_mat_a[i]["name"] for i in range(len(C.names_mat_a))]
+            names_f = [C.names_mat_f[i]["name"] for i in range(len(C.names_mat_f))]
+            names_m = [C.names_mat_m[i]["name"] for i in range(len(C.names_mat_m))]
         if player.participant.task == 'realeffort':
-            names_h = [C.names_re_h[i]["name"] for i in range(len(C.names_re_h))]
-            names_a = [C.names_re_a[i]["name"] for i in range(len(C.names_re_a))]
+            names_f = [C.names_re_f[i]["name"] for i in range(len(C.names_re_f))]
+            names_m = [C.names_re_m[i]["name"] for i in range(len(C.names_re_m))]
         return dict(
-            asian1=names_a[0],
-            asian2=names_a[1],
-            asian3=names_a[2],
-            asian4=names_a[3],
-            asian5=names_a[4],
-            asian6=names_a[5],
-            asian7=names_a[6],
-            asian8=names_a[7],
-            asian9=names_a[8],
-            asian10=names_a[9],
-            asian11=names_a[10],
-            asian12=names_a[11],
-            hispanic1=names_h[0],
-            hispanic2=names_h[1],
-            hispanic3=names_h[2],
-            hispanic4=names_h[3],
-            hispanic5=names_h[4],
-            hispanic6=names_h[5],
-            hispanic7=names_h[6],
-            hispanic8=names_h[7],
-            hispanic9=names_h[8],
-            hispanic10=names_h[9],
-            hispanic11=names_h[10],
-            hispanic12=names_h[11],
+            female1=names_f[0],
+            female2=names_f[1],
+            female3=names_f[2],
+            female4=names_f[3],
+            female5=names_f[4],
+            female6=names_f[5],
+            female7=names_f[6],
+            female8=names_f[7],
+            female9=names_f[8],
+            female10=names_f[9],
+            female11=names_f[10],
+            female12=names_f[11],
+            male1=names_m[0],
+            male2=names_m[1],
+            male3=names_m[2],
+            male4=names_m[3],
+            male5=names_m[4],
+            male6=names_m[5],
+            male7=names_m[6],
+            male8=names_m[7],
+            male9=names_m[8],
+            male10=names_m[9],
+            male11=names_m[10],
+            male12=names_m[11],
                 )
 
 
